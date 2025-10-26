4cde6f81c471790055892fb0f5ec485c1e4bd7a8
zijing02 <109534061+zijing02@users.noreply.github.com>
2025-10-26 15:24:36 +0800

    4.1

---
+++ 4.1/综合系统开发课程设计/参考/第1节/第1节/src/main/java/com/zhlx/ZhlxApplication.java
@@ -0,0 +1,13 @@
+package com.zhlx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhlxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhlxApplication.class, args);
+    }
+
+}
---
+++ 4.1/综合系统开发课程设计/参考/第1节/第1节/src/test/java/com/zhlx/ZhlxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhlx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhlxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
